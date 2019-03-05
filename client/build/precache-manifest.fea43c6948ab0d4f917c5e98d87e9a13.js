d7722866df427607cea525880f4af693ec85cc3d
Jim Girardeau <jwgirardeau@gmail.com>
2019-03-04 21:18:16 -0600

    initial

---
+++ client/build/precache-manifest.fea43c6948ab0d4f917c5e98d87e9a13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "66e89f6cbceaf6ffbaa8",
+    "url": "/static/js/main.66e89f6c.chunk.js"
+  },
+  {
+    "revision": "b1b59baa7e7cda293337",
+    "url": "/static/js/2.b1b59baa.chunk.js"
+  },
+  {
+    "revision": "66e89f6cbceaf6ffbaa8",
+    "url": "/static/css/main.e4f594f7.chunk.css"
+  },
+  {
+    "revision": "3af55496e040986b48aada423a3c8c7e",
+    "url": "/index.html"
+  }
+];
